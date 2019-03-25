5a5e53bfd1d2c41d878f0f90825c4f739e9f87c8
karanbhalla <kbhalla22@gmail.com>
2019-03-25 10:31:18 -0400

    Tested project for receiving a single customer

--- spring-crm-rest/src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
+++ spring-crm-rest/src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,23 +12,44 @@ import com.luv2code.springdemo.entity.Customer;
 import com.luv2code.springdemo.service.CustomerService;
 
 @RestController
-@RequestMapping("/api ")
+@RequestMapping("/api")
 public class CustomerRestController {
-	
-	//Autowire the cx service
+
+	// autowire the CustomerService
 	@Autowired
 	private CustomerService customerService;
 	
-	//add mapping for GET/customer
-@GetMapping("/customers")
-public List<Customer> getCustomers(){
-	return customerService.getCustomers();
-	
-	
-	
-	
-	
-	
+	// add mapping for GET /customers
+	@GetMapping("/customers")
+	public List<Customer> getCustomers() {
+		
+		return customerService.getCustomers();
+		
+	}
+		
+	// add mapping for GET/customers/{customerId}
+	@GetMapping("/customers/{customerId}")
+	public Customer getCustomer(@PathVariable int customerId) {
+		Customer theCustomer=customerService.getCustomer(customerId);
+		return theCustomer;
+	}
 	
 }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
